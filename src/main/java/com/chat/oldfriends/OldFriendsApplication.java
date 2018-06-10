d8d38cfc3616455a12d824fed98fd7363a2332d0
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2018-06-10 19:41:17 -0300

    first commit

---
+++ src/main/java/com/chat/oldfriends/OldFriendsApplication.java
@@ -0,0 +1,12 @@
+package com.chat.oldfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldFriendsApplication.class, args);
+	}
+}
